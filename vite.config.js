adcd35ab769a23a9b0a4258efc3429b715f01c51
pranavgit135 <khandekarpranav52@gmail.com>
2025-02-02 23:09:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'voice-assistance', // Replace 'repo-name' with your actual repository name
+  base: '/voice-assistance/', // Replace 'repo-name' with your actual repository name
 });
